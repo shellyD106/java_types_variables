e3ae6d1e423cdfc8ad6a7c1bf78e0e9d013cb604
Shelda Delassin <sheldadelassin@sheldas-mbp-2.home>
2021-02-17 23:06:16 -0500

    first commit

---
+++ src/typesVariablesandBranching/Main.java
@@ -0,0 +1,33 @@
+package typesVariablesandBranching;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner (System.in);
+//		String practice = userInput.nextLine();
+		
+//		int practice = userInput.next();	//we cannot convert from string to int because the method is giving us a string in order to get integer from user we have to use another method
+//		System.out.println(practice);
+		
+//		System.out.println(hello_world);	//I'm trying to print the value of hello world before I've declared it . The variable needs to be declared before it can be used
+//		int hello_world = 5;
+		
+		
+//		char e = '9u' 						//char can only be one character 
+
+		
+//		int y;
+//		System.out.println(y); 				//the variable y have not been initialized its only been declared. I need to initialize variable before I try to run it
+
+		
+//		int x = 5;
+//		double y = 8;
+//		int s = x * y;						//we can't convert from double to int,  were trying to say that the integer u is equal to x*y but y is a double value and when we get value back its going to be double so we can't convert that into an integer just by doing this
+		
+		
+	
+	}
+
+}
